382f9b34d588494e51ac268ca7470a9df89eda3e
Your Name~ <you@example.com>
2023-12-19 17:22:32 -0300

    Primer-Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Importa FormsModule
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
-  declarations:
-  [AppComponent,
-    ],
-  imports:
-   [BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    DashboardModule,
-  ], // Agrega FormsModule a los imports
+  declarations:[],
+  imports:[], 
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule {}
